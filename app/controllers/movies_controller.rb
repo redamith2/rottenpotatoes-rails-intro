53b3bcc1777a31643af3cb0851305cd3ea291e62
EC2 Default User <ec2-user@ip-172-31-29-83.us-east-2.compute.internal>
2018-09-21 05:07:31 +0000

    completed part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  
+  helper_method :sort_column, :sort_dir
 
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
@@ -11,9 +13,42 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if sort_dir != ""
+      @movies = Movie.order("#{sort_column} #{sort_dir}").all
+      if sort_column == "title"
+        @val = "hilite"
+        @val2 = ""
+      else
+        @val = ""
+        @val2 = "hilite"
+      end
+    else
+      @movies = Movie.all
+      @val = ""
+      @val2 = ""
+    end
   end
-
+  
+  def sort_column
+    if params[:column] == "Movie Title"
+      return "title"
+    elsif params[:column] == "Release Date"
+      return "release_date"
+    else
+      return "title"
+    end
+  end
+  
+  def sort_dir
+    if params[:dir] == "asc"
+      return "asc"
+    elsif params[:dir] == "desc"
+      return "desc"
+    else
+      return ""
+    end
+  end
+  
   def new
     # default: render 'new' template
   end
@@ -23,6 +58,8 @@ class MoviesController < ApplicationController
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
+  
+  
 
   def edit
     @movie = Movie.find params[:id]
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,16 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  
+  def sort_hyperlink(column)
+    column2 = (column == "Movie Title")? "title":"release_date"
+    dir = (column2 == sort_column && sort_dir == "asc")? "desc":"asc"
+    ident = ""
+    if column2 == "title"
+      ident = "title_header"
+    else
+      ident = "release_date_header"
+    end
+    link_to "#{column.titleize}",{column: column, dir: dir}, :id => ident
+  end
 end
